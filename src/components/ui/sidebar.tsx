c8ac026ff8bc9f5cff3347b5e281af85ca44d6a4
汪昕穎 <singing@sixdots.com.tw>
2025-10-02 09:10:08 +0000

    /Manager/ForgotPassword 不需要 <div data-sidebar="sidebar" class="flex h-fu

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -246,12 +246,7 @@ const Sidebar = React.forwardRef<
           )}
           {...props}
         >
-          <div
-            data-sidebar="sidebar"
-            className="flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
-          >
-            {children}
-          </div>
+          {children}
         </div>
       </div>
     )
