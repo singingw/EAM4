394608e4f3612e7afafcdbc20884d36a5f094138
汪昕穎 <singing@sixdots.com.tw>
2025-10-09 04:16:47 +0000

    !!!!!!!!!!!!!!!! (_for element h1_)

--- src/app/Manager/page.tsx
+++ src/app/Manager/page.tsx
@@ -2,7 +2,7 @@ export default function ManagerPage() {
   return (
     <div className="flex flex-1 items-center justify-center rounded-lg">
       <div className="text-center">
-        <h1 className="text-2xl font-bold text-foreground">歡迎來到 [系統名稱]-管理系統</h1>
+        <h1 className="text-2xl font-bold text-foreground">!!!!!!!!!!!!!!!!</h1>
         <p className="mt-2 text-foreground/80">請從左側選單選擇一個項目開始。</p>
       </div>
     </div>
