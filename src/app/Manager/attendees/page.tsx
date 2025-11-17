69d0f544b670362b57015197f3e9ef34bc7791ac
汪昕穎 <singing@sixdots.com.tw>
2025-11-17 07:20:26 +0000

    不用外框與底色 (_for element <Button>_)

--- src/app/Manager/attendees/page.tsx
+++ src/app/Manager/attendees/page.tsx
@@ -119,7 +119,7 @@ export default function AttendeesPage() {
             </Button>
             <Dialog>
               <DialogTrigger asChild>
-                <Button variant="outline" size="icon">
+                <Button variant="ghost" size="icon">
                   <Settings className="h-4 w-4" />
                 </Button>
               </DialogTrigger>
