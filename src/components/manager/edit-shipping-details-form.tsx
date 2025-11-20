a96c0ce7a8b8e5b69a5942c2e8a71c10cb2c9f8b
謝雨潔 <claudia@sixdots.com.tw>
2025-11-20 08:36:59 +0000

    欄位名稱調整為「資產編號」 (_for element <TableHead>_)

--- src/components/manager/edit-shipping-details-form.tsx
+++ src/components/manager/edit-shipping-details-form.tsx
@@ -701,7 +701,7 @@ export function EditShippingDetailsForm() {
                             <TableHead className="min-w-[150px]">名稱</TableHead>
                             <TableHead className="w-[120px]">倉庫</TableHead>
                             <TableHead className="w-[80px]">数量</TableHead>
-                            <TableHead className="w-[120px]">序號</TableHead>
+                            <TableHead className="w-[120px]">資產編號</TableHead>
                             <TableHead className="min-w-[150px]">備註</TableHead>
                             <TableHead className="w-[120px]">放置地點</TableHead>
                             <TableHead className="w-[120px]">存貨/備品/缺貨</TableHead>
@@ -837,3 +837,5 @@ export function EditShippingDetailsForm() {
     </>
   );
 }
+
+    
